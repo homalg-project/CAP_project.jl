a8d22f7dec32b569432a59bc9e9f34f82afcf22b
Kamal Saleh <kamal.saleh@web.de>
2026-09-23 13:06:43 +0200

    Update to GAP's CartesianCategories v2026.09-01
    Bump Version to v0.4.4

--- CartesianCategories/src/gap/InitialCategory.gi.autogen.jl
+++ CartesianCategories/src/gap/InitialCategory.gi.autogen.jl
@@ -69,3 +69,26 @@
     return I;
     
 end ) );
+
+##
+@InstallMethod( SetOfObjects,
+          "for initial CAP category",
+          [ IsInitialCapCategory ],
+          
+  function ( I )
+    
+    return SetOfObjectsOfCategory( I );
+    
+end );
+
+##
+@InstallMethod( SetOfGeneratingMorphisms,
+          "for initial CAP category",
+          [ IsInitialCapCategory ],
+          
+  function ( I )
+    
+    return SetOfGeneratingMorphismsOfCategory( I );
+    
+end );
+
